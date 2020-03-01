003a160ed5b6bb51469fa3eb27b2d9b0387b352e
sifatMoonjerin <smsrizon@gmail.com>
2020-03-01 12:06:22 +0600

    decrement function implemented

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 
 
+
 var increment = document.getElementsByClassName("fa-plus");
 
 Array.from(increment).map((el,index) => {
-    el.addEventListener('click', ()=> {
+    el.parentElement.addEventListener('click', ()=> {
         var quantity = document.getElementById(`quantity-${index}`).value;
         newQuantity = parseInt(quantity) + 1;
         document.getElementById(`quantity-${index}`).value = newQuantity;
         quantity = newQuantity
 })
+})
+
+
+var decrement = document.getElementsByClassName("fa-minus");
+
+Array.from(decrement).map((el,index) => {
+    el.parentElement.addEventListener('click', ()=> {
+        var quantity = document.getElementById(`quantity-${index}`).value;
+        newQuantity = parseInt(quantity) - (parseInt(quantity) > 0? 1 : 0);
+        document.getElementById(`quantity-${index}`).value = newQuantity;
+        quantity = newQuantity
+});
 })
